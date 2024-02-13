e1bf7f1e12bad53683019ac2cb4e8dc54bc29117
progamerxml <progamerxml@outlook.com>
2024-02-13 08:13:23 +0700

    hapus tanda merge - index.php

--- public/index.php
+++ public/index.php
@@ -6,13 +6,13 @@ use IRFANM\PHP\PORTOFOLIO\Controller\HomeController;
 use IRFANM\PHP\PORTOFOLIO\Controller\Testing;
 use IRFANM\PHP\PORTOFOLIO\Middleware\AuthMiddleware;
 
-<<<<<<< HEAD
+
 Router::add("GET", "/", HomeController::class, "index", []);
 Router::add("GET", "/test", Testing::class, "test", ["testMid"]);
 Router::add("GET", "/test/([0-9a-zA-Z]*)/id/([0-9a-zA-Z]*)", Testing::class, "testParameter", ["testMid"]);
-=======
+
 Router::add("GET", "/test", Testing::class, "test", []);
 Router::add("GET", "/test/([0-9a-zA-Z]*)/id/([0-9a-zA-Z]*)", Testing::class, "testParameter", [AuthMiddleware::class]);
->>>>>>> cd6d7e1d44aad5c041f4257684025c0401d94762
+
 
 Router::run();
